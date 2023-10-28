d56e7d61b8d72c2d95d7a136e94840b59f6f0245
tonypleite <portfolioantoniopleite@gmail.com>
2023-10-28 21:10:08 +0100

    The Physics of AI Reources
    Time:
    
    1.Time

---
+++ Assets/Time/Script/SecondsUpdate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondsUpdate : MonoBehaviour
+{
+    float timeStartOffset = 0;
+    bool goStartTime = false;
+
+    void Update()
+    {
+        if (!goStartTime)
+        {
+            timeStartOffset = Time.realtimeSinceStartup;
+            goStartTime = true;
+        }
+        this.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, Time.realtimeSinceStartup - timeStartOffset);
+    }
+}
