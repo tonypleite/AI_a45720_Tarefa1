4152c8d59f43e5779c07cc5d818f384a2ca2003b
tonypleite <portfolioantoniopleite@gmail.com>
2023-10-31 04:13:09 +0000

    The Physics of AI
    Acceleration:
    
    1.Acceleration Part 1
    2.Acceleration Part 2
    3.Acceleration Part 3

--- Assets/Scripts/MoveShell.cs
+++ Assets/Scripts/MoveShell.cs
@@ -2,19 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveShell : MonoBehaviour
-{
-    float speed = 1.0f;
+public class MoveShell : MonoBehaviour {
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 1.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        this.transform.Translate (0, Time.deltaTime * speed * 0.5f, Time.deltaTime * speed);
+
+    void Update() {
+
+        transform.Translate(0.0f, 0.0f, Time.deltaTime * speed);
     }
 }
