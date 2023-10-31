15ce33d9ffe45959ca90d3a42b4f2442ff94e0b0
tonypleite <portfolioantoniopleite@gmail.com>
2023-10-31 14:59:08 +0000

    Crowd Simulation
    Moving As One:
    
    1.Moving As One

---
+++ Assets/Crowd Simulation/Moving As One/Script/AIControl.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour
+{
+    public GameObject goal;
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.SetDestination (goal.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
