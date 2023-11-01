fefaeda2a319feef61dbb2f18bf2a3ac75a06cda
tonypleite <portfolioantoniopleite@gmail.com>
2023-11-01 07:29:51 +0000

    Crowd Simulation
    Creating a City Crowd:
    
    1.Creating a City Crowd Part 1

---
+++ Assets/AIControl.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour {
+
+    GameObject[] goalLocations;
+    NavMeshAgent agent;
+    Animator anim;
+
+    void Start() 
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        goalLocations = GameObject.FindGameObjectsWithTag("goal");
+        int i = Random.Range(0, goalLocations.Length);
+        agent.SetDestination(goalLocations[i].transform.position);
+        anim = this.GetComponent<Animator>();
+        anim.SetTrigger("isWalking");
+    }
+
+
+    void Update() 
+    {
+        if (agent.remainingDistance < 1)
+        {
+            int i = Random.Range(0, goalLocations.Length);
+            agent.SetDestination(goalLocations[i].transform.position);
+        }
+    }
+}
