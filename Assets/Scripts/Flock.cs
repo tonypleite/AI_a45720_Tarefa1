2a837a385c2677dabdda8f37592bfb24bf608e01
tonypleite <portfolioantoniopleite@gmail.com>
2023-11-02 19:48:03 +0000

    Crowd Simulation
    Flocking:
    
    1.Flocking Part 1

---
+++ Assets/Scripts/Flock.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(FlockManager.FM.minSpeed, FlockManager.FM.maxSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate (0, 0, speed * Time.deltaTime);
+    }
+}
