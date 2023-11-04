ff2a2068555bf9cc61790456f0518020b8b5dc96
tonypleite <portfolioantoniopleite@gmail.com>
2023-11-04 14:09:27 +0000

    Waypoints & Graphs
    Waypoints:
    
    2.Slerping to the Direction of Travel

--- Assets/Scripts/FollowWP.cs
+++ Assets/Scripts/FollowWP.cs
@@ -7,6 +7,7 @@ public class FollowWP : MonoBehaviour
     public GameObject[] waypoints;
     int currentWP = 0;
     public float speed = 10.0f;
+    public float rotSpeed = 10.0f;
     
     // Start is called before the first frame update
     void Start()
@@ -27,7 +28,12 @@ public class FollowWP : MonoBehaviour
             currentWP = 0;
         }
 
-        this.transform.LookAt(waypoints[currentWP].transform);
+        //this.transform.LookAt(waypoints[currentWP].transform);
+
+        Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
+
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
+
         this.transform.Translate(0, 0, speed * Time.deltaTime);
     }
 }
