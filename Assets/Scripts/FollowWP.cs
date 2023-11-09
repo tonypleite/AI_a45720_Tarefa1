98a559493c2fcf135ed1c40ad45a7e850176b4cc
tonypleite <portfolioantoniopleite@gmail.com>
2023-11-09 12:27:38 +0000

    Waypoints & Graphs
    Following a Circuit&Tracker:
    
    2.Following a Tracker

--- Assets/Scripts/FollowWP.cs
+++ Assets/Scripts/FollowWP.cs
@@ -6,19 +6,28 @@ public class FollowWP : MonoBehaviour
 {
     public GameObject[] waypoints;
     int currentWP = 0;
+
     public float speed = 10.0f;
     public float rotSpeed = 10.0f;
+    public float lookAhead = 40.0f;
+
+    GameObject tracker; 
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        DestroyImmediate(tracker.GetComponent<Collider>());
+        tracker.GetComponent<MeshRenderer>().enabled = false;
+        tracker.transform.position = this.transform.position;
+        tracker.transform.rotation = this.transform.rotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    void ProgressTracker()
     {
-        if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 3)
+        if (Vector3.Distance(tracker.transform.position, this.transform.position) > lookAhead) return;
+
+        if (Vector3.Distance(tracker.transform.position, waypoints[currentWP].transform.position) < 3)
         {
             currentWP++;
         }
@@ -28,7 +37,16 @@ public class FollowWP : MonoBehaviour
             currentWP = 0;
         }
 
-        //this.transform.LookAt(waypoints[currentWP].transform);
+        tracker.transform.LookAt(waypoints[currentWP].transform);
+        tracker.transform.Translate(0, 0, (speed + 4) * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProgressTracker();
+
+        this.transform.LookAt(waypoints[currentWP].transform);
 
         Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
 
